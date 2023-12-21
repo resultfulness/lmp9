17c9dce3dbf61a38bb271b8c068ec3208a9b7b54
Emilia Glibowska <emilia.glibowska@elmark.com.pl>
2023-12-21 13:30:15 +0100

    added main row selection

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,45 @@
 #include "gauss.h"
 
+int switch_rows(Matrix* mat, Matrix* b, int row_a, int row_b) {
+    if(mat->r != b->r)
+        return 2;
+    if(mat->r - 1 < row_a || mat->r - 1 < row_b)
+        return 1;
+    double tmp;
+    int i;
+    for(i = 0; i < mat->c; i++) {
+        tmp = mat->data[row_a][i];
+        mat->data[row_a][i] = mat->data[row_b][i];
+        mat->data[row_b][i] = tmp;
+    }
+    tmp = b->data[row_a][0];
+    b->data[row_a][0] = b->data[row_b][0];
+    b->data[row_b][0] = tmp;
+    return 0;
+}
+
 int eliminate(Matrix* mat, Matrix* b) {
     int i, j, k;
     double tmp;
     if(mat->r != mat->c || mat->r != b->r)
         return 2;
     int s = mat->r;
+    int max_index;
+    double max_value;
 
     for(k = 0; k < s; k++) {
         for(j = s - 1; j > k; j--) {
+            max_index = k;
+            max_value = mat->data[k][k];
+            for(i = k + 1; i < s; i++) {
+                tmp = mat->data[i][k];
+                if(max_value > tmp)
+                    continue;
+                max_index = i;
+                max_value = tmp;
+            }
+            if(max_index != k)
+                switch_rows(mat, b, k, max_index);
             if(mat->data[k][k] == 0)
                 return 1;
             tmp = mat->data[j][k] / mat->data[k][k];
--- src/gauss.h
+++ src/gauss.h
@@ -4,6 +4,12 @@
 #include "mat_io.h"
 
 /**
+ * Zwraca 0 - zamiana zakończona sukcesem
+ * Zwraca 1 - zamieniane indeksy poza rozmiarem macierzy
+ * Zwraca 2 - macierze o nieprawidłowych rozmiarach
+ */
+int switch_rows(Matrix* mat, Matrix* b, int row_a, int row_b);
+/**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  * Zwraca 2 - macierze o nieprawidłowych rozmiarach
